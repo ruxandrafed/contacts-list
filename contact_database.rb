ce5eff1b718a02003f666927ad30a8355fa5825d
Ruxandra F. <ruxandra.fediuc@gmail.com>
2015-09-08 16:52:09 -0700

    Minor fix to output string that asks for input

--- contact_database.rb
+++ contact_database.rb
@@ -1,10 +1,14 @@
 require 'csv'
+
 class ContactDatabase
+
   class << self
+
     # Reads content from @filename CSV and returns array of entries
     def read
       CSV.read('contacts.csv')
     end
+
     # Takes an array of contacts and writes them to file; automatically assigns id
     def write(arr)
       starting_id = read.length
