af86fba0f2d1fc8461b9a5d8589a9ebb19b4dac3
Ruxandra F. <ruxandra.fediuc@gmail.com>
2015-09-08 16:55:12 -0700

    Refactored code

--- contact.rb
+++ contact.rb
@@ -6,17 +6,14 @@ class Contact
   attr_accessor :name, :email
 
   def initialize(name, email)
-    # TODO: assign local variables to instance variables
     @name = name
     @email = email
   end
 
   def to_s
-    # TODO: return string representation of Contact
     "Name: #{@name}, Email: #{email}"
   end
 
-  ## Class Methods
   class << self
 
     # Initializes a contact and adds it to the list of contacts
@@ -27,27 +24,24 @@ class Contact
       ContactDatabase.write([@name, @email, @phone_numbers])
     end
 
-    # Fnds and returns contacts that contain the term in the first name, last name or email
+    # Finds and returns contacts that contain the term in the first name, last name or email
     def find(term)
-      all_contacts = ContactDatabase.read
-      matches = all_contacts.select {|entry| entry.to_s.include? (term)}
+      matches = ContactDatabase.read.select {|entry| entry.to_s.include? (term)}
     end
 
     # Return the list of contacts, as is
     def all
-      all_contacts = ContactDatabase.read
+      ContactDatabase.read
     end
 
     # Shows a contact based on ID
     def show(id)
-      all_contacts = ContactDatabase.read
-      matches = all_contacts.select {|entry| entry[0] == id.to_s}
+      matches = ContactDatabase.read.select {|entry| entry[0] == id.to_s}
     end
 
     # Checks if an entry with the same email address already exits in the database
     def contact_exists?(email)
-      all_contacts = ContactDatabase.read
-      same_email_matches = all_contacts.select {|entry| entry[2].include?(email)}
+      same_email_matches = ContactDatabase.read.select {|entry| entry[2].include?(email)}
       same_email_matches.empty? ? false : true
     end
 
