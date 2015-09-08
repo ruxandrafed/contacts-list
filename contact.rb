16491c45100e233bad371c03d0ffcd881c29244f
ruxandrafed <ruxandra.fediuc@gmail.com>
2015-09-07 23:28:18 -0700

    Tasks 1-5 done

--- contact.rb
+++ contact.rb
@@ -1,33 +1,48 @@
+require 'csv'
+require 'pry'
+
 class Contact
- 
+
   attr_accessor :name, :email
 
   def initialize(name, email)
     # TODO: assign local variables to instance variables
+    @name = name
+    @email = email
   end
- 
+
   def to_s
     # TODO: return string representation of Contact
+    "Name: #{@name}, Email: #{email}"
   end
- 
+
   ## Class Methods
   class << self
     def create(name, email)
       # TODO: Will initialize a contact as well as add it to the list of contacts
+      @name = name
+      @email = email
+      ContactDatabase.write([@name, @email])
     end
- 
+
     def find(term)
       # TODO: Will find and return contacts that contain the term in the first name, last name or email
+      all_contacts = ContactDatabase.read
+      matches = all_contacts.select {|entry| entry.to_s.include? (term)}
     end
- 
+
     def all
       # TODO: Return the list of contacts, as is
+      ContactDatabase.read.each{|arr| arr.join(", ")}
     end
-    
+
     def show(id)
       # TODO: Show a contact, based on ID
+      all_contacts = ContactDatabase.read
+      matches = all_contacts.select {|entry| entry[0] == id.to_s}
+      puts "Name: #{matches[0][1]}, email: #{matches[0][2]}"
     end
-    
+
   end
- 
+
 end
--- contact_list.rb
+++ contact_list.rb
@@ -1,5 +1,57 @@
-require_relative 'contact'
 require_relative 'contact_database'
+require_relative 'contact'
 
 # TODO: Implement command line interaction
 # This should be the only file where you use puts and gets
+
+@filename = 'contacts.csv'
+
+class Application
+
+  def initialize
+      @command = ARGV[0]
+      @command_param = ARGV[1]
+  end
+
+  def run_command
+    case @command
+    when "help"
+      print_help
+    when "new"
+      contact_info = get_contact_info
+      Contact.create(contact_info[1], contact_info[2])
+    when "list"
+      puts Contact.all
+    when "show"
+      Contact.show(@command_param)
+    when "find"
+      matches = Contact.find(@command_param)
+      matches.each {|match| puts "Name: #{match[1]}, email: #{match[2]}"}
+    end
+  end
+
+  #private
+  def print_help
+    puts "Here is a list of available commands:"
+    puts "\t new  - Create a new contact"
+    puts "\t list - List all contacts"
+    puts "\t show - Show a contact"
+    puts "\t find - Find a contact"
+  end
+
+  # Gets from $stdin the name and email for a new contact, returns array [id, name, email]
+  def get_contact_info
+    puts "Enter contact name:"
+    name = $stdin.readline().chomp
+    puts "Enter email address:"
+    email = $stdin.readline().chomp
+    id = ContactDatabase.read.length + 1
+    return [id, name, email]
+  end
+
+end
+
+Application.new.run_command
+
+# a = ContactDatabase.write([12, 'Numele','abc@abc.com'])
+
